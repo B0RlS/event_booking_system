8bc3fb76b5d66e2604795830ab916b16f57185e0
B0RlS <Boris.Tsarikov@gmail.com>
2025-02-18 16:06:04 +0100

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  root to: 'events#index'
+
   namespace :api do
     namespace :v1 do
-      devise_for :users
-
       resources :events, only: %i[index show] do
         resources :tickets, only: [:create]
       end
